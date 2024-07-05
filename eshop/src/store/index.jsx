879e925cfb9a5ce06c4ca97607d545b36ef03c17
Johnson Epo <145839868+johnsonepo@users.noreply.github.com>
2024-07-05 11:41:08 +0400

    Delete eshop directory

--- eshop/src/store/index.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from './cart'
-
-const store = configureStore({
-    reducer:{
-        cart: cartReducer,
-    }
-});
-export default store;
